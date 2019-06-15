c4bb9c44e2dccc468a1ce83ea0f03e5a59c0caaf
luccas <you@example.com>
2019-06-14 23:50:33 -0300

    Listing the github repositories with hooks

--- src/App.js
+++ src/App.js
@@ -1,5 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 export default function App() {
-  return <h1>First commit !</h1>;
+  const [repositories, setRepositores] = useState([]);
+
+  useEffect(async () => {
+    const response = await fetch("https://api.github.com/users/gaearon/repos");
+    const data = await response.json();
+    setRepositores(data);
+  }, []);
+
+  useEffect(() => {
+    const filtered = repositories.filter(repo => repo.favorite);
+    document.title = `Você tem ${filtered.length} favoritos.`;
+  }, [repositories]);
+
+  function handleFavorite(id) {
+    const newRepositories = repositories.map(repo => {
+      return repo.id === id ? { ...repo, favorite: !repo.favorite } : repo;
+    });
+
+    setRepositores(newRepositories);
+  }
+
+  return (
+    <>
+      <ul>
+        {repositories.map(repo => (
+          <li key={repo.id}>
+            {repo.name}
+            {repo.favorite && <span>(Favorito)</span>}
+            <button onClick={() => handleFavorite(repo.id)}>
+              {repo.favorite ? "Desfazer" : "Favoritar"}
+            </button>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
